a47f054a73f5bce64656648a4dba2213d9d047c7
Michael Riccelli <michaelriccelli@ons-fvff82l0q6lr.myfiosgateway.com>
2022-03-22 15:12:07 -0400

    Initial setup of file structure and header section css and javascript

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+window.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
